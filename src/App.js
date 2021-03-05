4ae239c065f7a36886fc79548890609cb5f365bd
Matheus Pedro <matheus.pedro1214@gmail.com>
2021-03-05 17:00:33 -0300

    aula17

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
 
 import CompPadrao, { Comp1, Comp2 } from './components/Multi';
 import Primeiro from './components/Primeiro'
 
 export default () => (
-    <SafeAreaView>
+    <SafeAreaView style={style.App}>
         <CompPadrao />
         <Comp1 />
         <Comp2 />
         <Primeiro />
     </SafeAreaView>
 )
+
+const style = StyleSheet.create({
+    App:{
+        flexGrow: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    }
+})
