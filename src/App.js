36557d39f64a525748690167395636a2459d264d
Matheus Pedro <matheus.pedro1214@gmail.com>
2021-03-05 12:29:21 -0300

    Aula13

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+function App(){
+    return (
+    <Text>
+        Primeiro Componente
+    </Text>
+    )
+}
+
+// function App(){
+//     const jsx = <Text>Primeiro Componente</Text>
+//     return jsx
+// }
+
+export default App;
