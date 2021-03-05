e659240925621f465eeeb0207634542b694f9866
Matheus Pedro <matheus.pedro1214@gmail.com>
2021-03-05 12:56:44 -0300

    aula15

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native';
 
-function App () {
-    return <Text>Primeiro Componente</Text>
-}
+import Primeiro from './components/Primeiro'
 
-// Formas possiveis de criação de Componentes:
-
-// export default function () {
-//     return <Text>Primeiro Componente</Text>
-// }
-
-// const App = function () {
-//     return <Text>Primeiro Componente!</Text>
-// }
-
-// export default () => {
-//     return <Text>Primeiro Componente!!</Text>
-// }
-
-// export default () => <Text>Primeiro Componente!!!</Text>
-
-export default App;
+export default () => (
+    <SafeAreaView>
+        <Primeiro />
+    </SafeAreaView>
+)
