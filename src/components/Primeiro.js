e2410ba8018a3f1897a0d454b86db567865b523a
Matheus Pedro <matheus.pedro1214@gmail.com>
2021-03-05 17:02:13 -0300

    aula18

--- src/components/Primeiro.js
+++ src/components/Primeiro.js
@@ -2,4 +2,9 @@ import React from 'react';
 import { Text } from "react-native"
 import Estilo from './estilo'
 
-export default () => <Text style={Estilo.txtG}>Primeiro!</Text>
+export default () => {
+    console.warn('Opa!')
+    return (
+        <Text style={Estilo.txtG}>Primeiro!</Text>
+    )
+}
