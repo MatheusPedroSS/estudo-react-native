f849ee86f7293ba3917f576e9450fb2f5b86b9c8
Matheus Pedro <matheus.pedro1214@gmail.com>
2021-03-05 13:05:19 -0300

    Aula16

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
 
+import CompPadrao, { Comp1, Comp2 } from './components/Multi';
 import Primeiro from './components/Primeiro'
 
 export default () => (
     <SafeAreaView>
+        <CompPadrao />
+        <Comp1 />
+        <Comp2 />
         <Primeiro />
     </SafeAreaView>
 )
